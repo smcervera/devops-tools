c4eb80bd84900cfc8dae825acdf0721349174298
smcervera <sergio.m.cervera@gmail.com>
2024-01-03 11:07:35 +0100

    init commit

---
+++ kubernates-config/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.24.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.12.1"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"  # Ruta al archivo de configuración de Kubernetes para Docker Desktop
+  config_context = "docker-desktop"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "kubernetes_namespace" "local" {
+  metadata {
+    name = "devops"
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "jenkins_pvc" {
+  metadata {
+    name = "jenkins-pvc-local"
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "10Gi"
+      }
+    }
+  }
+}
+
+resource "helm_release" "argocd" {
+  chart            = "argo-cd"
+  name             = "argocd"
+  namespace        = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  create_namespace = true
+}
+
+resource "helm_release" "argocd-apps" {
+  depends_on = [helm_release.argocd]
+  chart      = "argocd-apps"
+  name       = "argocd-apps"
+  namespace  = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+
+  values = [
+    file("argocd/applications.yaml")
+  ]
+}
